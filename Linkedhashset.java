913451150e298441e35d5ca0d16e52195ac0487a
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-16 22:47:46 +0530

    Create Linkedhashset.java

---
+++ Linkedhashset.java
@@ -0,0 +1,15 @@
+//collection LinkedHashset
+import java.util.*;  
+public class TestJavaCollection8{  
+public static void main(String args[]){  
+LinkedHashSet<String> set=new LinkedHashSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
